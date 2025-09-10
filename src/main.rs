69b03c8e12e89612520535da5a850059af8ddb3c
bishnu7babu <mbishnu7799@gmail.com>
2025-09-10 09:44:32 +0530

    added Sha1Hash

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,26 @@
-use md5;
 use clap::Parser;
-use std::{io::{self, BufRead}, path::PathBuf};
-use sha1;
+use std::{
+    io::{self, BufRead},
+    path::PathBuf,
+};
+use md5;
+use sha1::{Digest, Sha1};
 
 #[derive(Parser)]
 #[command(version, about, long_about = None)]
 struct Cli {
     #[arg(short = 'f', long, value_name = "FILE", required = true)]
-    path: PathBuf,
+    file: PathBuf,
 
     #[arg(long, value_name = "HASH", required = true)]
     hash: String,
+
+    #[arg(short, long, value_name = "MODE", required = true, help = "md5, sha1")]
+    mode: String,
 }
 
 trait HashAlgorithm {
+    fn name(&self) -> &str;
     fn hash(&self, input: &[u8]) -> Vec<u8>;
     fn hash_hex(&self, input: &[u8]) -> String {
         self.hash(input).iter().map(|b| format!("{:02x}", b)).collect()
@@ -21,43 +28,63 @@ trait HashAlgorithm {
 }
 
 struct Md5Hash;
-
 impl HashAlgorithm for Md5Hash {
+    fn name(&self) -> &str {
+        "MD5"
+    }
     fn hash(&self, input: &[u8]) -> Vec<u8> {
         let hash = md5::compute(input);
         hash.0.to_vec()
     }
 }
 
+struct Sha1Hash;
+impl HashAlgorithm for Sha1Hash {
+    fn name(&self) -> &str {
+        "SHA1"
+    }
+    fn hash(&self, input: &[u8]) -> Vec<u8> {
+        let mut hasher = Sha1::new();
+        hasher.update(input);
+        hasher.finalize().to_vec()
+    }
+}
+
 fn main() {
-
     let cli = Cli::parse();
-    let hash = cli.hash;
-    let hasher = Md5Hash;
 
-    let file = match std::fs::File::open(&cli.path) {
-        Ok(f) => f,
-        Err(e) => {
-            eprintln!("Error reading file {}: {}", cli.path.display(), e);
+    // choose hasher based on mode
+    let hasher: Box<dyn HashAlgorithm> = match cli.mode.as_str() {
+        "md5" => Box::new(Md5Hash),
+        "sha1" => Box::new(Sha1Hash),
+        _ => {
+            eprintln!("Unsupported mode: {}", cli.mode);
             return;
         }
     };
 
-    let reader = io::BufReader::new(&file);
+    let file = match std::fs::File::open(&cli.file) {
+        Ok(f) => f,
+        Err(e) => {
+            eprintln!("Error reading file {}: {}", cli.file.display(), e);
+            return;
+        }
+    };
 
-    for (i,line) in reader.lines().enumerate() {
+    let reader = io::BufReader::new(file);
+
+    for (i, line) in reader.lines().enumerate() {
         match line {
             Ok(word) => {
                 let computed_hash = hasher.hash_hex(word.as_bytes());
-                if computed_hash == hash {
+                if computed_hash == cli.hash {
                     println!("✅ Match found at line {}: {}", i + 1, word);
                     return;
                 }
-            },
-            Err(e) => eprintln!("Error reading line {}",e)
+            }
+            Err(e) => eprintln!("Error reading line {}: {}", i + 1, e),
         }
     }
 
-    let input: &[u8] = b"hello";
-    let hash = hasher.hash(input);
+    println!("❌ No match found.");
 }
